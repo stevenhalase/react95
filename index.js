18548bbfdcd6add40669d06b5ff19f81f050957e
Steve <steven@nsrit.com>
2018-05-18 11:00:08 -0500

    Adjusted packagejson scripts and config to serve static storybook

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const path = require('path');
 
 const port = process.env.PORT;
 
-app.use(express.static(__dirname + '/build'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/storybook/index.html'));
+});
+
+app.get('/storybook', function(req, res) {
+    res.sendFile(path.join(__dirname + '/storybook/index.html'));
+});
+
+app.use(express.static(__dirname + '/storybook'));
 
 server.listen(port, () => {
     console.log('Server started at http://localhost:' + port);
